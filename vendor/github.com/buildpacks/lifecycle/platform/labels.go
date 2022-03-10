eb926578676a7c184ed0215bfa46626919d26525
Robert Wittman <robkwittman@gmail.com>
2022-03-10 16:02:21 -0500

    go mod tidy

--- vendor/github.com/buildpacks/lifecycle/platform/labels.go
+++
@@ -1,9 +0,0 @@
-package platform
-
-const (
-	BuildMetadataLabel   = "io.buildpacks.build.metadata"
-	LayerMetadataLabel   = "io.buildpacks.lifecycle.metadata"
-	ProjectMetadataLabel = "io.buildpacks.project.metadata"
-	StackIDLabel         = "io.buildpacks.stack.id"
-	MixinsLabel          = "io.buildpacks.stack.mixins"
-)
